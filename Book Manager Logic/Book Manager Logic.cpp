25568595e4c081111e52c4927fd495a2ada9bafb
Kacper Wesołowski <kacper.wesolowski@student.pk.edu.pl>
2025-05-18 14:54:58 +0200

    Added subproject responsible for logic

---
+++ Book Manager Logic/Book Manager Logic.cpp
@@ -0,0 +1,10 @@
+﻿// Book Manager Logic.cpp : Definiuje funkcje biblioteki statycznej.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: To jest przykład funkcji biblioteki
+void fnBookManagerLogic()
+{
+}
